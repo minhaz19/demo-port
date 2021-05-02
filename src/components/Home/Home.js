ff438a85c9c0ef99575921ddd75e31c8cfc3d89a
Minhaz19 <minhazulabedin44@gmail.com>
2021-05-02 17:40:25 +0600

    done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div className="row">
+            <div className="col-sm-1 col-md-1"></div>
+            <div className="col-sm-11 col-md-11">This is for Home</div>
+        </div>
+    );
+};
+
+export default Home;
